edfebb317e92cd426ae942a38461b99f80c93409
arango34 <77129869+arango34@users.noreply.github.com>
2021-01-07 20:37:09 -0500

    Add project files.

---
+++ razorpages/Migrations/20210107005656_AddISBNToBookModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace razorpages.Migrations
+{
+    public partial class AddISBNToBookModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
